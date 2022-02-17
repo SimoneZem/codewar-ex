12803a18bfcfba4e91ba40d2e308f69fad2f8db3
Simone Trita <simonetrita@gmail.com>
2022-02-17 22:14:37 +0100

    feat(add explanationEx1): add explanation for ex1

--- src/App.jsx
+++ src/App.jsx
@@ -3,8 +3,9 @@ import { FunctionInOrder } from "./components/FunctionInOrder";
 import "./App.css";
 
 const sequence = [];
-let iterable = "AAAABBBCCDAABBB";
+let iterable = "AAAABBBCCDAABBB"; //dichiaro la funzione iterable
 for (let i = 0; i < iterable.length; i++) {
+  // inizializzo i con valore 0, la condizione è che i < della quantità di caratteri, incremente di 1
   if (iterable[i] !== iterable[i + 1]) {
     sequence.push(iterable[i]);
   }
